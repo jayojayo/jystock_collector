14c7b9934ffd9d778a13ac52967128c030d8d20c
jayojin <jyjang69@naver.com>
2023-03-07 12:55:14 +0900

    업데이트

--- naver/get_1bong.py
+++ naver/get_1bong.py
@@ -4,6 +4,8 @@ import pandas as pd
 from sqlalchemy import create_engine
 import pymysql
 from tqdm import tqdm
+import sys
+import datetime
 
 
 class Collecting:
@@ -14,28 +16,51 @@ class Collecting:
         self.get_company_list()
         self.stock_df = pd.DataFrame()
 
-        split_list = list(range(0,len(self.company_list),200))
+        self.today = datetime.date.today().strftime('%Y-%m-%d')
+        self.progress_check()
+
+        split_list = list(range(self.i_1+200, len(self.company_list), 200))
         split_list.append(len(self.company_list))
 
-        i_1 = 0
         for i in split_list:
+            print(i)
             self.stock_df_total = pd.DataFrame([], columns=['code', 'name', 'date', 'open', 'high', 'low', 'close',
                                                             'volume'])
-            for code in tqdm(list(self.company_list['종목코드'])[i_1:i]):
+            for code in tqdm(list(self.company_list['종목코드'])[self.i_1:i]):
                 self.get_price(code)
                 self.stock_df_total = pd.concat([self.stock_df_total, self.stock_df], ignore_index=True)
-                i_1 = i
+                self.i_1 = i
 
             self.stock_df_total['date'] = pd.to_datetime(self.stock_df_total['date'], format='%Y%m%d')
 
-            if i == 500:
-                self.stock_df_total.to_sql(name='stock_daily', con=self.db_connection, if_exists='replace', index=False)
+            if i == 200:
+                self.stock_df_total.to_sql(name='stock_daily', con=self.engine, if_exists='replace', index=False)
             else:
-                self.stock_df_total.to_sql(name='stock_daily', con=self.db_connection, if_exists='append', index=False)
+                self.stock_df_total.to_sql(name='stock_daily', con=self.engine, if_exists='append', index=False)
+            self.cur.execute(f"UPDATE check_table SET date='{self.today}', progress={i} WHERE id=1")
+            self.connection.commit()
 
     def db_connect(self):
-        self.db_connection = create_engine('mysql+pymysql://jystock:wkdwls1992@localhost/stock')
-        conn = self.db_connection.connect()
+        self.engine = create_engine('mysql+pymysql://jystock:wkdwls1992@localhost/stock')
+        self.conn = self.engine.connect()
+        self.connection = pymysql.connect(host='localhost', user='jystock', password='wkdwls1992', database='stock')
+        self.cur = self.connection.cursor()
+
+    def progress_check(self):
+        self.cur.execute("SELECT * FROM information_schema.tables WHERE table_schema='stock' AND table_name='check_table'")
+        table_exists = bool(self.cur.fetchone())
+
+        if not table_exists:
+            self.i_1 = 0
+            self.cur.execute("CREATE TABLE check_table (id INT AUTO_INCREMENT PRIMARY KEY, date DATE, progress INT)")
+            self.cur.execute(f"INSERT INTO check_table(id, date, progress) VALUES(1,'{self.today}',{self.i_1})")
+            self.date = self.today
+            self.connection.commit()
+        else:
+            self.cur.execute("SELECT date, progress FROM check_table WHERE id=1")
+            result = self.cur.fetchone()
+            self.date = result[0]
+            self.i_1 = result[1]
 
     def get_price(self, company_code):
         headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'}
